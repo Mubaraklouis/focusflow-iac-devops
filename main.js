d53a93da65b024a9bc8abc4739d35f5db00d3f17
mubarak <luismubarak@gmail.com>
2025-05-14 14:29:18 +0200

    update to v1.3.0

---
+++ main.js
@@ -0,0 +1,20 @@
+function main() {
+    const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question('Please enter your name : ', input => {
+    console.log(`Hello ${input}!`);
+    console.log('Welcome to the world of Node.js');
+    console.log('This is a simple program to demonstrate the use of readline module.');
+    console.log('You can enter your name and the program will greet you.');
+    console.log('You can also enter any other text and the program will echo it back to you.');
+    console.log('This is a simple program to demonstrate the use of readline module.');
+    console.log('You can enter your name and the program will greet you.');
+    console.log('You can also enter any other text and the program will echo it back to you.');         
+    readline.close();
+});
+ 
+}
+main();
